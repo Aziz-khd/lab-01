336f90e435a7f2ca8d96466b0e5189ccbf6308ca
Aziz-khd <khdouraziz@gmail.com>
2022-08-25 18:14:46 +0300

    first project

---
+++ main.dart
@@ -0,0 +1,25 @@
+void main(){
+  
+Map Abedalazizinfo ={
+  "first_name": "abedalaziz",
+  "mid_name": "Ahmad",
+  "last_name":"khdour",
+  "age":22,
+
+  "sports":["football","basketball,"],
+
+  "education info":{
+    "name":"yarmouk univeristy",
+    "faculty":"IT",
+    "major":"Computer Science",
+
+
+
+  }
+
+
+
+
+};
+print(Abedalazizinfo);
+}
